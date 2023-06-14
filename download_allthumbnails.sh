2d645f162f2b829937d1e1c56d188ae3151c3130
kenh0u <21225079+kenh0u@users.noreply.github.com>
2023-06-15 03:55:40 +0900

    fix download_allthumbnails.sh

--- download_allthumbnails.sh
+++ download_allthumbnails.sh
@@ -15,7 +15,19 @@ mkdir -p thumbnail
 
 while read line
 do
-  thumbnailurl='https://i.ytimg.com/vi/'"$line"'/hq720.jpg'
+  thumbnailurl='https://i.ytimg.com/vi/'"$line"'/maxresdefault.jpg'
   output='thumbnail/'"$line"'.jpg'
   wget -O "$output" "$thumbnailurl"
+  if [ $? -ne 0 ];then
+    thumbnailurl='https://i.ytimg.com/vi/'"$line"'/sddefault.jpg'
+    wget -O "$output" "$thumbnailurl"
+    if [ $? -ne 0 ];then
+      thumbnailurl='https://i.ytimg.com/vi/'"$line"'/hqdefault.jpg'
+      wget -O "$output" "$thumbnailurl"
+      if [ $? -ne 0 ];then
+        thumbnailurl='https://i.ytimg.com/vi/'"$line"'/mqdefault.jpg'
+        wget -O "$output" "$thumbnailurl"
+      fi
+    fi
+  fi
 done < videoids.txt
