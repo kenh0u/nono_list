81e35c64b7764afa9ecec281eb6a88796e8673c5
kenh0u <21225079+kenh0u@users.noreply.github.com>
2023-06-15 02:52:41 +0900

    add download_allchats.sh

---
+++ download_allchats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LOG=download_allchats.log
+
+exec 1> >(
+  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+    | tee -a $LOG
+)
+exec 2> >(
+  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+    | tee -a $LOG
+)
+
+mkdir -p chat
+
+while read line
+do
+  videourl='https://youtu.be/'"$line"
+  yt-dlp -o "chat/%(upload_date)s_%(title)s_[%(id)s].%(ext)s" --skip-download --youtube-skip-dash-manifest --write-sub --cookies youtube.com_cookies.txt "$videourl"
+done < videoids.txt
